d81218cf7c283c8dbac30ce76620dcda7c664b4a
kotamp <kinomorima@gmail.com>
2020-04-15 16:00:16 +0300

    tmux fish termux vim vnc w3m

---
+++ .config/fish/config.fish
@@ -0,0 +1,73 @@
+function la
+  ls -lah
+end
+
+function l 
+  ls -a
+end
+
+#function fish_prompt
+#`  echo (pwd) "> "
+#end
+
+function vifish
+  vim ~/.config/fish/config.fish
+end
+
+function refish
+  source ~/.config/fish/config.fish
+end
+
+function virc
+  vim ~/.vimrc
+end
+
+function w3mb
+  w3m ~/.w3m/bookmark.html
+end
+
+function download-site
+  wget \
+    --recursive \
+    --level=10 \
+    --no-clobber \
+    --page-requisites \
+    --html-extension \
+    --convert-links \
+    --restrict-file-names=windows \
+    $argv
+end
+
+function gulp
+  ./node_modules/.bin/gulp $argv
+end
+
+function nodemon
+  ./node_modules/.bin/nodemon $argv
+end
+
+function gs
+  git status
+end
+
+function ga
+  git add $argv
+end
+
+function gc
+  git commit -m $argv
+end
+
+function gpu
+  git push -u origin master
+end
+
+function setup-umgi
+  git config user.email "gtdsocial@icloud.com"
+  git config user.name  "umgi"
+end
+
+function setup-kotamp
+  git config user.email "kinomorima@gmail.com"
+  git config user.name "kotamp"
+end
